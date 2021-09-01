e15e09e8d226c399fcf91af861c5c000babe60ee
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-31 22:04:44 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -10,23 +10,22 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
-server.applyMiddleware({ app });
-
+server.start().then(() => {
+  server.applyMiddleware({ app });
+ });
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+  app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
 });
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,9 +1,7 @@
 const jwt = require('jsonwebtoken');
-
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
-
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function ({req}) {
@@ -16,7 +14,6 @@ module.exports = {
     if (!token) {
       return req;
     }
-
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
@@ -25,7 +22,6 @@ module.exports = {
       console.log('Invalid token');
       return res.status(400).json({ message: 'invalid token!' });
     }
-
     // send to next endpoint
     next();
   },
