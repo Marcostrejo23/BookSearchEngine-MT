febd3ccfc352bf23bbd6c04d4adb445d6dfbe5c5
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-31 21:52:14 -0700

    mutations and queries done

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -12,8 +12,6 @@ const SavedBooks = () => {
   const userData = data?.me || {};
   const [deleteBook, { error } ]= useMutation(REMOVE_BOOK);
     };
-
-
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -72,7 +70,6 @@ const SavedBooks = () => {
       </Container>
     </>
   );
-};
 
 
 export default SavedBooks;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+const { gql } = require('@apollo/client');
+
+export const GET_ME = gql`
+  {  
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        link
+        image
+        title
+      }
+    }
+  }
+`;
